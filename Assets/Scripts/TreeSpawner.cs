fa7bc329296a65bd926f40476b49ca4e448c4a06
Hologuardian <namaeskram@gmail.com>
2016-01-30 17:45:50 -0500

    Nodiness acomplisheded
    altar, bonfire, houses, need to add farmling things

--- Assets/Scripts/TreeSpawner.cs
+++ Assets/Scripts/TreeSpawner.cs
@@ -12,15 +12,14 @@ public class TreeSpawner : MonoBehaviour {
         {
             RaycastHit hit;
 
-            Vector2 unit = Random.insideUnitCircle * 1000;
-            Ray r = new Ray(transform.position + new Vector3(unit.x, 0, unit.y), new Vector3(0, -1, 0));
-            Physics.Raycast(r, out hit);
-            int x = (int)(((unit.x + 1000) / 2000) * terrain.terrainData.alphamapWidth);
-            int y = (int)(((unit.y + 1000) / 2000) * terrain.terrainData.alphamapHeight);
+            Vector2 unit = Random.insideUnitCircle * 1000 + new Vector2(1000, 1000);
+            Vector3 point = new Vector3(unit.x, terrain.SampleHeight(new Vector3(unit.x, 0, unit.y)), unit.y);
+            int x = (int)(((unit.x) / 2000) * terrain.terrainData.alphamapWidth);
+            int y = (int)(((unit.y) / 2000) * terrain.terrainData.alphamapHeight);
             float a = terrain.terrainData.GetAlphamaps(x, y, 1, 1)[0, 0, 0];
             Debug.Log(x + " " + y + " " + terrain.terrainData.alphamapWidth + " " + terrain.terrainData.alphamapHeight + " " + a);
             if (a > 0.8f)
-                Instantiate(tree, hit.point, transform.rotation);
+                Instantiate(tree, point, transform.rotation);
         }
 	}
 	
