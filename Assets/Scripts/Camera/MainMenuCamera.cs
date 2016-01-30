4b369489e8ef6231c78c0ffd86412bce773aeab6
Houshikan Yokeswaran <hau5tastic@gmail.com>
2016-01-29 22:22:51 -0500

    started main menu scene

---
+++ Assets/Scripts/Camera/MainMenuCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuCamera : MonoBehaviour {
+
+    float rotationY = 0.0f;
+    Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+        mainCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = Quaternion.Euler(330.0f, rotationY, 0.0f);
+
+        rotationY += 0.15f;
+
+        if(rotationY == 360.0f)
+        {
+            rotationY = 0.0f;
+        }
+    }
+}
