72309df625656e508048a4fb6a33a41ad8934102
Hologuardian <namaeskram@gmail.com>
2016-01-30 18:45:21 -0500

    Buildings and trees and bushes oh my!
    Buildings properly rotated and placed
    bushes spawning
    test trees spawning at right height,.

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -4,16 +4,21 @@ using System.Collections;
 public class Building : MonoBehaviour
 {
     public Vector3 Size;
+    public GameObject model;
+    public float Scale;
+    public Transform target;
     // Use this for initialization
     void Start()
     {
-
+        GameObject building = Instantiate(model, transform.position, Quaternion.AngleAxis(Random.value * 360, Vector3.up)) as GameObject;
+        building.transform.localScale = new Vector3(Scale, Scale, Scale);
+        building.transform.LookAt(new Vector3(target.position.x, building.transform.position.y, target.position.z));
     }
 
     void OnDrawGizmos()
     {
-        Gizmos.color = Color.black;
-        Gizmos.DrawCube(transform.position, Size);
+        //Gizmos.color = Color.black;
+        //Gizmos.DrawCube(transform.position, Size);
     }
 
     // Update is called once per frame
--- Assets/Scripts/TreeSpawner.cs
+++ Assets/Scripts/TreeSpawner.cs
@@ -5,20 +5,20 @@ public class TreeSpawner : MonoBehaviour {
 
     public GameObject tree;
     public Terrain terrain;
+    public float verticalOffset;
+    public int spawnNumber = 5000;
+    public float determ = 0.8f;
 	// Use this for initialization
 	void Start () 
     {
-        for (int i = 0; i < 5000; ++i)
+        for (int i = 0; i < spawnNumber; ++i)
         {
-            RaycastHit hit;
-
             Vector2 unit = Random.insideUnitCircle * 1000 + new Vector2(1000, 1000);
-            Vector3 point = new Vector3(unit.x, terrain.SampleHeight(new Vector3(unit.x, 0, unit.y)), unit.y);
+            Vector3 point = new Vector3(unit.x, terrain.SampleHeight(new Vector3(unit.x, 0, unit.y)) - verticalOffset, unit.y);
             int x = (int)(((unit.x) / 2000) * terrain.terrainData.alphamapWidth);
             int y = (int)(((unit.y) / 2000) * terrain.terrainData.alphamapHeight);
             float a = terrain.terrainData.GetAlphamaps(x, y, 1, 1)[0, 0, 0];
-            Debug.Log(x + " " + y + " " + terrain.terrainData.alphamapWidth + " " + terrain.terrainData.alphamapHeight + " " + a);
-            if (a > 0.8f)
+            if (a > determ)
                 Instantiate(tree, point, transform.rotation);
         }
 	}
