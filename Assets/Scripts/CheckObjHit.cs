4b80edffb6ba655762b4c9a753c55fcc098edb06
Daniel Argento <daniel.argento94@hotmail.com>
2016-01-30 15:13:38 -0500

    Delayed Comitt
    Implemented:
    -MainMenu
    -Shitty blood particle effect
    - GetObjectClicked class

---
+++ Assets/Scripts/CheckObjHit.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public int layerMask = -1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    GameObject GetClickedObj()
+    {
+        //cast ray from main camera, starting from where mouse was clicked
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        //cast ray, return object it hit
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+        {
+            hit.transform.gameObject.BroadcastMessage("onClick");
+            return hit.transform.gameObject;
+        }
+
+        else
+            return null;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject clickedgmobj = null;
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickedgmobj = GetClickedObj();
+
+            //display true
+            if (clickedgmobj != null) 
+            Debug.Log(clickedgmobj.name);
+        }
+        
+	
+	}
+}
---
+++ Assets/Scripts/menuScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class menuScript : MonoBehaviour {
+
+
+    public Button startText;
+    public Button exitText;
+
+	// Use this for initialization
+	void Start () {
+
+        startText = startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+	}
+
+    public void exitPress()
+    {
+        Application.Quit();
+    }
+
+    public void startPress()
+    {
+        Application.LoadLevel(1);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
