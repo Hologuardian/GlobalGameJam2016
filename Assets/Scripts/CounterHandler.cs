a947cca97696cd5f5516fce5b82254d4a7fcffb0
Hologuardian <namaeskram@gmail.com>
2016-01-31 15:49:35 -0500

    Shaders
    bloom and contrast stretch

---
+++ Assets/Scripts/CounterHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CounterHandler : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start ()
+    {
+        AudioHandler.Instance.PlayBackgroundMusic(AudioHandler.BackgroundMusic.BGMTRIBAL);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        
+	}
+}
