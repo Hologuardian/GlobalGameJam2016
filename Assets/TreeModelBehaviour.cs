130771621de4807b0296c13f5bc7921080946013
Justin <jbelair91@gmail.com>
2016-01-31 07:18:05 -0500

    Cleaned up mess upon mess of Box001 and Sphere003
    All new models are prefabbed and textured. If anyone names another piece
    of something Box001 I will literally stab them in the left testicle.

---
+++ Assets/TreeModelBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeModelBehaviour : MonoBehaviour
+{
+    public Renderer Trunk1;
+    public Renderer Canopy1;
+    public Renderer Trunk2;
+    public Renderer Canopy2;
+    public Renderer Trunk3;
+    public Renderer Canopy3;
+    // Use this for initialization
+    void Start()
+    {
+        int i = Random.Range(0, 2);
+
+        if (i == 0)
+        {
+            Trunk1.enabled = Canopy1.enabled = true;
+            Trunk2.enabled = Canopy2.enabled = false;
+            Trunk3.enabled = Canopy3.enabled = false;
+        }
+        else if (i == 1)
+        {
+            Trunk1.enabled = Canopy1.enabled = false;
+            Trunk2.enabled = Canopy2.enabled = true;
+            Trunk3.enabled = Canopy3.enabled = false;
+        }
+        else
+        {
+            Trunk1.enabled = Canopy1.enabled = false;
+            Trunk2.enabled = Canopy2.enabled = false;
+            Trunk3.enabled = Canopy3.enabled =true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/VariantModelBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VariantModelBehaviour : MonoBehaviour {
+    public Renderer[] variants;
+	// Use this for initialization
+	void Start () {
+        foreach (Renderer rend in variants) {
+            rend.enabled = false;
+
+        }
+
+        variants[Random.Range(0, variants.Length - 1)].enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
