60389a804f3aeb3f64fd09eaacca3f8f922a9580
Hologuardian <namaeskram@gmail.com>
2016-01-31 11:25:27 -0500

    Lighting and final terrain locations

--- Assets/Scripts/TreeSpawner.cs
+++ Assets/Scripts/TreeSpawner.cs
@@ -1,36 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
-public class TreeSpawner : MonoBehaviour {
+[Serializable]
+public class Determ
+{
+    public int Sample = 0;
+    public float Determinator = 0.5f;
+    public bool less = false;
+}
+
+public class TreeSpawner : MonoBehaviour
+{
 
     public GameObject tree;
     public Terrain terrain;
     public float verticalOffset;
     public int spawnNumber = 5000;
-    public float determ = 0.8f;
-    public int SampleVal = 0;
-	// Use this for initialization
-	void Start () 
+    public Determ[] determ;
+    // Use this for initialization
+    void Start()
     {
         for (int i = 0; i < spawnNumber; ++i)
         {
-            Vector2 unit = Random.insideUnitCircle * 1000 + new Vector2(1000, 1000);
+            Vector2 unit = UnityEngine.Random.insideUnitCircle * 1000 + new Vector2(1000, 1000);
             Vector3 point = new Vector3(unit.x, terrain.SampleHeight(new Vector3(unit.x, 0, unit.y)) - verticalOffset, unit.y);
             int x = (int)(((unit.x) / 2000) * terrain.terrainData.alphamapWidth);
             int y = (int)(((unit.y) / 2000) * terrain.terrainData.alphamapHeight);
-            float a = terrain.terrainData.GetAlphamaps(x, y, 1, 1)[0, 0, SampleVal];
-            if (a > determ)
+            bool spawn = false;
+            var alphaMap = terrain.terrainData.GetAlphamaps(x, y, 1, 1);
+            foreach (Determ d in determ)
             {
-                Vector2 rand = Random.insideUnitCircle;
+                float a = alphaMap[0, 0, d.Sample];
+                if(d.less)
+                {
+                    if (a <= d.Determinator && a > 0)
+                    {
+                        spawn = true;
+                    }
+                }
+                else
+                {
+                    if (a > d.Determinator)
+                    {
+                        spawn = true;
+                    }
+                }
+            }
+            if (spawn)
+            {
+                Vector2 rand = UnityEngine.Random.insideUnitCircle;
                 GameObject obj = Instantiate(tree, point, Quaternion.LookRotation(new Vector3(rand.x, 0, rand.y))) as GameObject;
-                obj.transform.localScale = new Vector3((Random.value * 0.5f) + 0.75f, (Random.value * 0.2f) + 0.9f, (Random.value * 0.2f) + 0.9f);
+                float width = (UnityEngine.Random.value * 0.2f) + 0.9f;
+                obj.transform.localScale = new Vector3((UnityEngine.Random.value * 0.5f) + 0.75f, width, width);
                 obj.transform.parent = transform;
             }
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
