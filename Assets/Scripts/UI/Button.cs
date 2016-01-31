81980268210b7b0d44cd5310fa4e3c7c7f49a937
Houshikan Yokeswaran <hau5tastic@gmail.com>
2016-01-30 21:18:15 -0500

    Button Polish
    added text fade on hover for people unfamiliar with the game

---
+++ Assets/Scripts/UI/Button.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Button : MonoBehaviour {
+
+    Text text;
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponentInChildren<Text>();
+        text.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void ShowText()
+    {
+        text.CrossFadeAlpha(0, 3.0f, true);
+        text.enabled = true;
+    }
+
+    void HideText()
+    {
+        text.enabled = false;
+        text.CrossFadeAlpha(1, 0.1f, true);
+    }
+}
